76648f021492f7e1e0f74914d1e4995ad8fb4e2a
Alex Smith <alex.smith@redhat.com>
2020-03-29 18:54:43 -0400

    Provide aggregate statistics for all source files within a directory.

--- codestats.py
+++ codestats.py
@@ -6,6 +6,8 @@ Currently this just checks for comments content using Java/C++ comment
 style syntax. Maybe someday it'll do more.
 """
 
+import os
+import os.path
 import re
 import sys
 
@@ -15,8 +17,8 @@ def pct(n, m, ndigits=0):
     return round(100 * n / float(m), ndigits)
 
 
-def summarize(source, re_prog):
-    """Display source code statistics."""
+def analyze(source, re_prog):
+    """Return statistics for source code."""
     comments = re_prog.findall(source)
     chunks = re_prog.split(source)
     comments_length = sum(len(s) for s in comments)
@@ -32,22 +34,52 @@ def summarize(source, re_prog):
             print('Chunk %d [CODE]:' % (i + 1))
         print (s + '\n')
 
-    print('Summary')
-    print('-------')
-    print('total length: %d' % source_length)
-    print('code length: %d' % code_length)
-    print('comments length: %d' % comments_length)
-    print('comments content percentage: %.1f%%' % comments_pct)
+    return {
+        'source_length': source_length,
+        'code_length': code_length,
+        'comments_length': comments_length,
+    }
+
+
+def analyze_file(file_path, re_prog):
+    """Return source code statistics for a source code file."""
+    with open(file_path) as f:
+        return analyze(f.read(), re_prog)
+
+
+def find_files(parent_dir):
+    """Recursively list files within a directory."""
+    for (dir_path, dir_names, file_names) in os.walk(parent_dir):
+        for file_name in file_names:
+            yield os.path.join(dir_path, file_name)
 
 
 def main(argv=None):
     """The main part."""
     argv = argv or sys.argv
-    filename = argv[1]
+    parent_dir = argv[1]
     multiline_prog = re.compile(r'(/\*.*?\*/|//.*?$)', (re.MULTILINE|re.DOTALL))
 
-    with open(filename) as f:
-        summarize(f.read(), multiline_prog)
+    source_files = (
+        fpath for fpath in find_files(parent_dir) if fpath.endswith('.scala')
+    )
+
+    stats = []
+    for fpath in source_files:
+        print('Analyzing', fpath)
+        stats.append(analyze_file(fpath, multiline_prog))
+
+    source_total = sum(x['source_length'] for x in stats)
+    code_total = sum(x['code_length'] for x in stats)
+    comments_total = sum(x['comments_length'] for x in stats)
+    comments_pct = pct(comments_total, source_total, 1)
+
+    print('Summary')
+    print('-------')
+    print('total source: %d' % source_total)
+    print('total code: %d' % code_total)
+    print('total comments: %d' % comments_total)
+    print('comments percentage: %.1f%%' % comments_pct)
 
 
 if __name__ == '__main__':
